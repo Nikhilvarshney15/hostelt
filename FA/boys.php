06324c5dc453032113ee786ee5209b993e159618
Nikhilvarshney15 <59953511+Nikhilvarshney15@users.noreply.github.com>
2022-02-11 11:02:05 +0530

    Add files via upload

---
+++ FA/boys.php
@@ -0,0 +1,289 @@
+
+
+
+<html>
+<head>
+ <title>book hostels::available for booking</title>
+		 <link rel="stylesheet"href="css/boys1.css">
+		 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+		 <link rel="stylesheet"href="res/resboys1.css">
+
+<meta name="viewport" content="width=device-width,initial-scale=1 user-scalable=no">
+
+</head>
+<body>
+<div id="top"><p id="date">
+  <script>
+    document.getElementById("date").innerHTML=Date();
+	</script>
+  </p>
+</div>
+<div id="header">
+<a href="index.php"> <img style="float:left;margin-top:10px;" src="image/logo.jpg"/></a>
+ 
+ <center><p>world luxery hostels</p></center>
+
+
+</div>
+<div class="togglearea">
+<label for="toggle">
+<span></span>
+<span></span>
+<span></span>
+
+</label>
+</div>
+ <input type="checkbox" id="toggle">
+
+<div class="navbar">
+  <a href="index.php">Home</a>
+ <div class="dropdown">
+    <button class="dropbtn">Services 
+    </button>
+    <div class="dropdown-content">
+      <a href="boys.php">Hostel For Boys</a>
+      <a href="#">PG For Boys</a>
+      <a href="girl.php">Hostel for Girls</a>
+	       <a href="#">PG for Girls</a>
+		        <a href="#">Flats</a>
+    </div>
+  </div>
+
+   <a href="#news">BookNow</a>
+<div class="dropdown">
+    <button class="dropbtn">Cities 
+     
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Aligarh</a>
+      <a href="#">Greater Noida</a>
+      <a href="#">Noida</a>
+	       <a href="#">Delhi</a>
+    </div>
+  </div>
+
+		     <a href="#">Contact</a>
+			
+
+</div>
+
+<div class="post_body">
+<font size="6" color="blue"><center>World of Luxary Hostels</center></font>
+
+
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="image/bnd1.jpg" style="width:100%;height:300px;">
+  <div class="text"><h1>Hostels/PG/Flats</h1></div>
+</div>
+
+<div class="mySlides fade" >
+  <div class="numbertext">2 / 3</div>
+  <img src="image/slide2.jpg" style="width:100%;height:300px;">
+  <div class="text"><h1>Hostels/PG/Flats</h1></div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="image/slide3.jpg" style="width:100%;height:300px;">
+  <div class="text"><h1>Hostels/PG/Flats<h1></div>
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+</div>
+<div class="post_body1">
+<font size="6" color="blue"><center>Boys hostels</center></font>
+ <div class="hostel1">
+   <a href="index.php"> <img src="image/hostel1.jpg" style="width:300px;height:200px;"></a>
+   <br>
+   <div class="texth">
+   <font><center>click here to see detail</center></font>
+   </div>
+     <button class="btn success">Success</button>
+
+</div>
+ <div class="hostel1">
+   <a href="index.php"> <img src="image/hostel1.jpg" style="width:300px;height:200px;"></a>
+   <br>
+   <div class="texth">
+   <font><center>click here to see detail</center></font>
+   </div>
+     <button class="btn success">Success</button>
+
+</div>
+<div class="hostel1">
+   <a href="index.php"> <img src="image/hostel1.jpg" style="width:300px;height:200px;"></a>
+   <br>
+   <div class="texth">
+   <font><center>click here to see detail</center></font>
+   </div>
+     <button class="btn success">Success</button>
+
+</div>
+<div class="hostel1">
+   <a href="index.php"> <img src="image/hostel1.jpg" style="width:300px;height:200px;"></a>
+   <br>
+   <div class="texth">
+   <font><center>click here to see detail</center></font>
+   </div>
+     <button class="btn success">Success</button>
+
+</div>
+<div class="hostel1">
+   <a href="index.php"> <img src="image/hostel1.jpg" style="width:300px;height:200px;"></a>
+   <br>
+   <div class="texth">
+   <font><center>click here to see detail</center></font>
+   </div>
+     <button class="btn success">Success</button>
+
+</div>
+<div class="hostel1">
+   <a href="index.php"> <img src="image/hostel1.jpg" style="width:300px;height:200px;"></a>
+   <br>
+   <div class="texth">
+   <font><center>click here to see detail</center></font>
+   </div>
+     <button class="btn success">Success</button>
+
+</div>
+</div>
+<div class="sidebar">
+<div class="getbox">
+<form method="post" action="bnd.php">
+<font size="6" color="white";><center>Schedule for visit</center></font>
+<input type="text"class="textsize" placeholder="Name" required><br>
+
+<input type="email" class="textsize" placeholder="E-mail"><br>
+<input type="text"  class="textsize" placeholder="Mobile Number" required><br>
+<input type="text" class="textsize" placeholder="Date of Visit / Today's Time"><br>
+<input type="text" class="textsize" placeholder="College Name" required>
+
+<br>
+<button  type="submit" value="send" class="sendb"><b>Send</b></button>
+<button id="myBtn1">Contact Us</button>
+</form>
+
+<form method="post" action="bnd.php">
+<div id="myModal1" class="modal1">
+
+  
+  <div class="modal-content1">
+    <span class="close1">&times;</span>
+    <p>CONTACT TO OWNER</p>
+	<p>NAME:</P>
+	<input type="text"class="textsize" name="name"placeholder=" Your Name:" required><br>
+	<P>COLLEGE NAME:</P>
+<input type="text"class="textsize" name="clg"placeholder=" Your College name:" required><br>
+<P>CONTACT NO:</P>
+<input type="text"class="textsize" name="number" placeholder="Your phone no:" required><br>
+<button  type="submit" value="send"class="sendb1"><b>Send</b></button>
+
+
+  </div>
+
+</div>
+</form>
+</div>
+</div>
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+var modal1 = document.getElementById('myModal1');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+
+</script>
+
+
+<div class="foot">
+<div class="aboutus">
+<div class="aboutus2">
+
+<font size="6">About Us</font>
+</div>
+<br>
+<font>Looking for "Best hostels in Greater Noida"? You are in the right place. with daily updating reviews and truth pics of all hostel.</font>
+<br>
+<br>
+<i class="fa fa-phone" aria-hidden="true"></i>
+<font>91+ 8533998500 , 91+ 7300553155</font>
+</div>
+
+<div class="aboutus">
+<div class="aboutus2">
+<font size="6">Quick links</font>
+</div>
+
+</div>
+<div class="aboutus">
+<div class="aboutus2">
+
+<font size="6">Stay in touch</font>
+</div>
+<br>
+<font>We are Social connect with us to get latest news and details of all hostels at PAN-INDIA Basis.</font>
+
+<ul>
+<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+</ul>
+
+
+</div>
+</div>
+</body>
+</html>
